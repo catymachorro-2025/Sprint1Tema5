9528993da5f8055d5e021d3568de681ca31f0449
Lourdes Catalina Machorro <catymachorro@gmail.com>
2025-10-15 10:42:17 +0200

    Ejercicio 1 agregando el tipo de animal y Ejercicio 2

--- Nivel1.php
+++ Nivel1.php
@@ -4,35 +4,81 @@ interface Sonido{
 }
 abstract class Animal implements Sonido {
     protected string $nombre;
-    public function __construct(string $nombre) {
+    protected string $tipo;
+    public function __construct(string $tipo, string $nombre) {
+        $this->tipo = $tipo;
         $this->nombre = $nombre;
     }
     public function presentacion() : string {
-        return "Soy $this->nombre mi sonido caracteristico es: ". $this->hablar()."\n";
-    }
+    
+    return "Me llamo $this->nombre, soy un $this->tipo, mi sonido caracteristico es: ". $this->hablar()."\n";
+}
 }
 class Perro extends Animal {
     public function hablar() : string {
-        return "Guau guau! Porque soy un perro";
+        return "Guau guau!";
     }
+  
 }
 class Pato extends Animal {
     public function hablar() : string {
-        return "Cuac cuac! porque soy un Pato";
+        return "Cuac cuac!";
     }
+ 
 }
+
 class Vaca extends Animal {
     public function hablar() : string {
-        return "Muuuu muuu porque soy una vaca";
+        return "Muuuu muuu!";
     }
+
 }
-$perro = new Perro("Max");
-$pato = new Pato("Donald");
-$vaca = new Vaca("Lala");
+$perro = new Perro("perro", "Max");
+$pato = new Pato("pato", "Donald");
+$vaca = new Vaca("vaca", "Lala");
 
 echo $perro->presentacion();
+
 echo $pato->presentacion();
+
 echo $vaca->presentacion();
+?>
+
+<?php 
+abstract class Shape {
+   protected float $ancho;
+   protected float $alto;
+
+   public function __construct($ancho,$alto){
+    $this->ancho = $ancho;
+    $this->alto = $alto;
+   }
+abstract public function calcularArea();
+
+}
+
+class Rectangulo extends Shape {
+    public function calcularArea() : float {
+        return  $this->ancho * $this->alto;
+    }
+   
+    }
+class Triangulo extends Shape {
+    public function calcularArea() : float{
+        return ($this->ancho * $this->alto)/2;
+}
+
+
+}
+$rectangulo1 = new Rectangulo(20,15);
+$triangulo1 = new Triangulo(12,22);
+
+
+$areaRectangulo1 = $rectangulo1->calcularArea();
+echo "El area del Rectangulo de 20x15 es: ".$areaRectangulo1. "\n";
+
+$areaTriangulo1 = $triangulo1->calcularArea();
+echo "El area del triangulo de 12x22 es: ".$areaTriangulo1."\n";
 
 
 ?>
